bceef4492c729f7571cbba0a0f0a460b6fc3dca9
Lukáš Zjavka <lukas.zjavka15@gmail.com>
2025-11-28 09:35:58 +0100

    Update randomelement.js
    One file with all functions for random elements generating

--- src/utilities/randomelement.js
+++ src/utilities/randomelement.js
@@ -1,3 +1,24 @@
 export function getRandomElement(array) {
     return array[Math.floor(Math.random() * array.length)];
 }
+
+export function getRandomGender() {
+  return Math.random() < 0.5 ? "male" : "female";
+}
+
+export function randomBirthday(minAge, maxAge) {
+  const now = Date.now();
+  const yearMs = 365.25 * 24 * 60 * 60 * 1000;
+
+  const youngest = now - minAge * yearMs;
+  const oldest   = now - maxAge * yearMs;
+
+  const randomTime = oldest + Math.random() * (youngest - oldest);
+
+  return new Date(randomTime).toISOString();
+}
+
+export function randomWorkload() {
+  const options = [10, 20, 30, 40];
+  return getRandomElement(options);
+}

5941cf98128ee6218a0b4343f0f2a17a0ca8005d
Lukáš Zjavka <lukas.zjavka15@gmail.com>
2025-11-28 09:55:49 +0100

    Update main.js
    Final edition with right imports

--- main.js
+++ main.js
@@ -1,29 +1,40 @@
-//TODO add imports if needed
-import {
-  maleNames,
-  femaleNames,
-  maleSurnames,
-  femaleSurnames,
-  workloads
-} from "./src/utilities/data.js";
+import { maleNames, femaleNames, maleSurnames, femaleSurnames, workloads } from "./src/utilities/data.js";
+import { getRandomElement, getRandomGender, randomBirthday, randomWorkload } from "./src/utilities/randomelement.js";
+import { validateAgeRange } from "./src/utilities/validation.js";
 
-import { getRandomElement } from "./src/utilities/randomelement.js";
-import { randomBirthDate } from "./src/utilities/randombirthday.js";
-
-//TODO doc
 /**
  * The main function which calls the application. 
- * Please, add specific description here for the application purpose.
+ * Generates a list of employees with random data.
  * @param {object} dtoIn contains count of employees, age limit of employees {min, max}
  * @returns {Array} of employees
  */
 export function main(dtoIn) {
-  //TODO code
+  const { count, age } = dtoIn;
+  const { min, max } = age;
 
+  validateAgeRange(min, max);
 
-  function personGenerator() {
+  const dtoOut = [];
 
-}
+  for (let i = 0; i < count; i++) {
+    const gender = getRandomGender();
+
+    const name = gender === "male"
+      ? getRandomElement(maleNames)
+      : getRandomElement(femaleNames);
+
+    const surname = gender === "male"
+      ? getRandomElement(maleSurnames)
+      : getRandomElement(femaleSurnames);
+
+    dtoOut.push({
+      gender,
+      birthdate: randomBirthday(min, max),
+      name,
+      surname,
+      workload: randomWorkload()
+    });
+  }
   
   //let dtoOut = exMain(dtoIn);
   return dtoOut;

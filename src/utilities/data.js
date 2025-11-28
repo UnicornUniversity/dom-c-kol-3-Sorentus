f3a081a83d56a295e5e3fc491899af0347e349f8
Lukáš Zjavka <lukas.zjavka15@gmail.com>
2025-11-28 09:03:38 +0100

    Update data.js

--- src/utilities/data.js
+++ src/utilities/data.js
@@ -1,11 +1,11 @@
-const mensNames = [
+const maleNames = [
     "Jan","Petr","Jakub","Tomáš","Lukáš","Matěj","Adam","Ondřej","Martin","Filip",
     "Vojtěch","Marek","Jiří","Karel","Michal","Daniel","David","Josef","Jaroslav","Roman",
     "Milan","Radek","Aleš","Patrik","Dominik","Šimon","Samuel","Robin","Vladimír","Radim",
     "Hynek","Vít","Sebastian","Erik","Richard","Rudolf","Bohuslav","Oldřich","Pavel","Libor",
     "Igor","Stanislav","Marcel","Eduard","Břetislav","Leoš","Alan","Vilém","Čeněk","Vlastimil"
 ];
-const mensSurnames = [
+const maleSurnames = [
     "Novák","Svoboda","Novotný","Dvořák","Černý","Procházka","Kučera","Veselý","Horák","Němec",
     "Marek","Pokorný","Král","Růžička","Beneš","Fiala","Sedláček","Doležal","Zeman","Kolář",
     "Urban","Kopecký","Čermák","Vaněk","Kříž","Pospíšil","Musil","Šimek","Říha","Steiner",
@@ -13,7 +13,7 @@ const mensSurnames = [
     "Vávra","Sýkora","Tichý","Vondráček","Kubík","Slavík","Ptáček","Holub","Hájek","Vacek"
 ];
 
-const womensNames = [
+const femaleNames = [
     "Lucie","Eva","Anna","Marie","Tereza","Adéla","Eliška","Natálie","Kristýna","Veronika",
     "Karolína","Barbora","Klára","Nikola","Markéta","Kateřina","Hana","Jana","Monika","Alena",
     "Rozálie","Sofie","Magdaléna","Gabriela","Simona","Denisa","Petra","Sandra","Zuzana","Nela",
@@ -21,7 +21,7 @@ const womensNames = [
     "Olga","Šárka","Blanka","Aneta","Michaela","Renata","Radka","Andrea","Sabina","Helena"
 ];
 
-const womensSurnames = [
+const femaleSurnames = [
     "Nováková","Svobodová","Novotná","Dvořáková","Černá","Procházková","Kučerová","Veselá","Horáková","Němcová",
     "Marková","Pokorná","Králová","Růžičková","Benešová","Fialová","Sedláčková","Doležalová","Zemanová","Kolářová",
     "Urbanová","Kopecká","Čermáková","Vaňková","Křížová","Pospíšilová","Musilová","Šimková","Říhová","Steinerová",

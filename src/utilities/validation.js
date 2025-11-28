9042987ced0f3285d942d60bf4966ca3b0239544
Lukáš Zjavka <lukas.zjavka15@gmail.com>
2025-11-28 10:28:22 +0100

    Delete src/utilities directory

--- src/utilities/validation.js
+++
@@ -1,8 +0,0 @@
-export function validateAgeRange(minAge, maxAge) {
-  if (minAge < 18 || maxAge > 60) {
-    throw new Error("Age interval must be within <18, 60>.");
-  }
-  if (minAge > maxAge) {
-    throw new Error("Invalid age interval: minAge cannot be greater than maxAge.");
-  }
-}
